e6f581e4c96390a59437fa78a9a2ec4107f6cf92
Joan <joanalemany26@gmail.com>
2024-08-27 09:14:17 +0200

    ejercicio 1 de php index1.php

---
+++ index1.php
@@ -0,0 +1,29 @@
+<?php
+$rooms = [
+    [
+        'ID' => '1231231',
+        'Name' => 'Deluxe124',
+        'Number' => '101',
+        'Price' => '1234',
+        'Discount' => '20'
+    ],
+    [
+        'ID' => '2234567',
+        'Name' => 'Suite567',
+        'Number' => '102',
+        'Price' => '2345',
+        'Discount' => '15'
+    ],
+    [
+        'ID' => '3456789',
+        'Name' => 'Standard789',
+        'Number' => '103',
+        'Price' => '3456',
+        'Discount' => '10'
+    ]
+];
+
+echo '<pre>';
+print_r($rooms);
+echo '</pre>';
+?>
