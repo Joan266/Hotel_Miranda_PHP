4817a94095370b7e795d8ed9538e2dc0ce58f5d6
Joan <joanalemany26@gmail.com>
2024-08-27 09:32:53 +0200

    ejercicio 3 de php index3.php

--- index4.php
+++ index4.php
@@ -34,8 +34,6 @@ foreach ($rooms as $room) {
     <p><strong>Rate:</strong> $<?php echo htmlspecialchars($foundRoom['rate']); ?></p>
     <p><strong>Status:</strong> <?php echo htmlspecialchars($foundRoom['status']); ?></p>
 <?php else: ?>
-    <p>No room found with ID <?php echo htmlspecialchars($id); ?>.</p>
-<?php endif; ?>
 
 </body>
 </html>
